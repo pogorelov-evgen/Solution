6388d112b83fc19787d828c7dff4017f44865d5b
pogorelov-evgen <72943262+pogorelov-evgen@users.noreply.github.com>
2023-11-01 16:25:26 +0300

    Maps

--- src/Solution.java
+++ src/Solution.java
@@ -1,14 +1,45 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+
+import static java.lang.System.currentTimeMillis;
 
 
 class Solution  {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) {
+     Map<Integer,String> hashMap = new HashMap<>();
+     Map<Integer,String> linkedHashMap = new LinkedHashMap<>();
+     Map<Integer,String> treeMap = new TreeMap<>();
 
-        PrintWriter pw = new PrintWriter(new File("test"));
-        pw.println("dfgh1234134");
-        pw.close();
+     testMap(hashMap);
+     testMap(linkedHashMap);
+     testMap(treeMap);
+
     }
+    public static void testMap(Map<Integer,String> map) {
+        long start = currentTimeMillis();
+        map.put(1,"den");
+        map.put(5,"den3");
+        map.put(51,"den4");
+        map.put(187,"den");
+        map.put(2,"den5");
+        map.put(38,"den");
+        map.put(89,"de6n");
+        map.put(1578,"7den");
+        map.put(67,"de8n");
+        map.put(32,"de7n");
+        map.put(643,"den");
+        map.put(5647,"d4en");
+        map.put(543,"de567n");
+        map.put(357,"de8hn");
+        map.put(365,"defgn");
+        long writeTime = currentTimeMillis() - start;
+        System.out.println(writeTime);
+        for(Map.Entry<Integer,String> entry:map.entrySet()){
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
+        System.out.println();
+        long readTime = currentTimeMillis() - writeTime;
+        System.out.println(readTime);
+    }
+
 }
