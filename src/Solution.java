c590a44007bfb3e054ff309deba7c8cb2d3121d7
pogorelov-evgen <72943262+pogorelov-evgen@users.noreply.github.com>
2023-11-01 12:01:17 +0300

    reader

--- src/Solution.java
+++ src/Solution.java
@@ -1,27 +1,14 @@
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-class Solution {
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        System.out.println(solution.firstUniqChar("leetcode"));
 
-    }
-    public int firstUniqChar(String s) {
-        char[] array = s.toCharArray();
-        int d = 1;
-        for (int i = 0; i < array.length; i++) {
-            d = 1;
-           for (int j = 0; j < array.length; j++) {
-               if (array[i] == array[j] && i!= j) {
-                   d=0;
-                   break;
-               }
-           }
-           if(d==1) return i;
+class Solution  {
+    public static void main(String[] args) throws FileNotFoundException {
 
-        }
-        return -1;
+        PrintWriter pw = new PrintWriter(new File("test"));
+        pw.println("dfgh1234134");
+        pw.close();
     }
 }
